97610980521f4c5dc8b2e5365532a6feda513a03
Sarah <115049982+sarnoire@users.noreply.github.com>
2023-04-29 20:19:18 -0600

    Adding/Changing API fixes to test in insomnia

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
 const port = 3000;
 const thoughtRoutes = require('./routes/thoughts');
 const reactionRoutes = require('./routes/reactions');
 const Thought = require('./models/thought');
 const Reaction = require('./models/reaction');
+const express = require('express');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const app = express();
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'My API',
+      version: '1.0.0',
+      description: 'API for my application',
+    },
+  },
+  // Path to the API docs
+  apis: ['./routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost/myapp', { useNewUrlParser: true });
